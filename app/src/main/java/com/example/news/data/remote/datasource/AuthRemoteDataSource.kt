97dbed232cd63d26b560b8b2a111bc5cb6db9066
Your Name <Your Name>
2023-05-26 11:40:31 +0300

    Implement Auth Repo Testing

--- app/src/main/java/com/example/news/data/remote/datasource/AuthRemoteDataSource.kt
+++ app/src/main/java/com/example/news/data/remote/datasource/AuthRemoteDataSource.kt
@@ -1,7 +1,5 @@
 package com.example.news.data.remote.datasource
 
-import com.example.news.domin.model.User
-
 interface AuthRemoteDataSource {
     suspend fun login(email:String,password:String) : RemoteUser?
     suspend fun signUP(email: String,userName:String,password: String) : Boolean
