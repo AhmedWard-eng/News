efb1afeb99be7855424cf9d76c5ade37a83a7c70
ahmed ward <ahmedward221@gmail.com>
2023-05-26 12:45:13 +0300

    apply PR changes

--- app/src/main/java/com/example/news/data/remote/network/RetrofitService.kt
+++ app/src/main/java/com/example/news/data/remote/network/RetrofitService.kt
@@ -1,5 +1,6 @@
 package com.example.news.data.remote.network
 
+import com.example.news.BuildConfig
 import com.example.news.data.remote.entity.AuthRequest
 import com.example.news.data.remote.entity.AuthResponse
 
@@ -9,7 +10,8 @@ import retrofit2.http.POST
 import retrofit2.http.Query
 
 
+private const val KEY = BuildConfig.API_KEY
 interface RetrofitService {
     @POST("/accounts:signInWithPassword")
-    suspend fun signUp(@Body authRequest: AuthRequest, @Query("key") key : String = "AIzaSyAVRNpZKTubdM9mp3L9HaE8XGj09eNoV1o") : AuthResponse
+    suspend fun signUp(@Body authRequest: AuthRequest, @Query("key") key : String = KEY) : AuthResponse
 }
