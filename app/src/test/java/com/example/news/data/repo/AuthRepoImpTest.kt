5e14945bd0397e5835abd8a0f03f2ed9419cfdea
Your Name <Your Name>
2023-05-26 09:44:58 +0300

    Implement Auth Repo (Create auth repoImp, Login,Sign up, Logout, Savelogged in data)_Changes2

--- app/src/test/java/com/example/news/data/repo/AuthRepoImpTest.kt
+++ app/src/test/java/com/example/news/data/repo/AuthRepoImpTest.kt
@@ -1,10 +1,7 @@
 package com.example.news.data.repo
 
 import org.junit.After
-import org.junit.Assert.*
 import org.junit.Before
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 
 class AuthRepoImpTest{
     private var authRepoImp : AuthRepoImp? = null
