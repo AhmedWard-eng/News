9bdd0542690d72453a477c25028a2217bdb3b12d
Yasmeen Ataa <yasmenataa7@gmail.com>
2023-05-26 14:25:23 +0300

    add viewModel dependencies Create LoginViewModelFactory.kt

--- app/src/main/java/com/example/news/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/news/ui/login/LoginViewModel.kt
@@ -1,4 +1,8 @@
 package com.example.news.ui.login
 
-class LoginViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.news.data.repo.AuthRepo
+
+class LoginViewModel (private val repo : AuthRepo): ViewModel() {
+
 }
