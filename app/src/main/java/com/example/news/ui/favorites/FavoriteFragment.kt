1aab899894283372f9824442260283f00f18466b
ahmed ward <ahmedward221@gmail.com>
2023-05-27 15:50:21 +0300

    search

--- app/src/main/java/com/example/news/ui/favorites/FavoriteFragment.kt
+++ app/src/main/java/com/example/news/ui/favorites/FavoriteFragment.kt
@@ -2,9 +2,14 @@ package com.example.news.ui.favorites
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -20,6 +25,8 @@ class FavoriteFragment : Fragment() {
     // TODO: Rename and change types of parameters
    lateinit var binding : FragmentFavoriteBinding
    private val viewModel : FavoritesViewModel by viewModels()
+    var list : List<News>? = null
+    var filteredList : List<News>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +34,6 @@ class FavoriteFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentFavoriteBinding.inflate(inflater,container,false)
-
         return binding.root
     }
 
@@ -35,14 +41,51 @@ class FavoriteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val favAdapter = FavAdapter(FavAdapter.OnClickListener(::deleteFromFav,::navigateToDetailsScreen))
         binding.recyclerView.adapter = favAdapter
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // This method is called before the text is changed.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                filteredList = list?.filter { it.title.contains(s.toString()) }
+                if(filteredList.isNullOrEmpty()){
+                        binding.groupNoMatchingResult.visibility = VISIBLE
+                        binding.recyclerView.visibility = GONE
+                }else{
+
+                    binding.groupNoMatchingResult.visibility = GONE
+                    binding.recyclerView.visibility = VISIBLE
+                }
+                favAdapter.submitList(filteredList)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // This method is called after the text has been changed.
+            }
+        }
+
+        binding.searchTextField.addTextChangedListener(textWatcher)
+
+
+
         lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
                 viewModel.news.collectLatest {
+                    list = it
+                    if(it.isEmpty()){
+
+                        binding.groupNoMatchingResult.visibility = VISIBLE
+                        binding.recyclerView.visibility = GONE
+                    }else{
+                        binding.groupNoMatchingResult.visibility = GONE
+                        binding.recyclerView.visibility = VISIBLE
+                    }
                     favAdapter.submitList(it)
                 }
             }
         }
-    }
+     }
 
     private fun navigateToDetailsScreen(news: News) {
         TODO("Not yet implemented")
